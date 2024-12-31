df2477a8ceceaaedecc6c3d3c9a6490bfd324fd9
Nagarjuna3e <148531051+Nagarjuna3e@users.noreply.github.com>
2024-12-31 19:09:07 +0530

    Add files via upload

---
+++ rhombus.java
@@ -0,0 +1,20 @@
+public class rhombus {
+    public static void main(String[] args) {
+        int num=3;
+    
+        for(int i=1;i<=num;i++)
+        {
+            for(int j=1;j<2*num;j++)
+            {
+                if(i>=j || i+j>=2*num)
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
